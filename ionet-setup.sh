74f58436d217b9fdd514d9440a25df8008c79542
Smiral Rashinkar <smiraldr@gmail.com>
2024-01-26 12:33:59 +0530

    Update ionet-setup.sh

--- ionet-setup.sh
+++ ionet-setup.sh
@@ -43,7 +43,8 @@ else
                                 sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
                                 sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
                                 sudo apt install linux-headers-$(uname -r) -y
-                                sudo apt del 7fa2af80
+				sudo apt del 7fa2af80 || true
+                                sudo apt remove 7fa2af80 || true
                                 sudo apt install build-essential cmake gpg unzip pkg-config software-properties-common ubuntu-drivers-common -y
                                 sudo apt install libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev -y || true
                                 sudo apt install libjpeg-dev libpng-dev libtiff-dev -y || true
@@ -53,7 +54,7 @@ else
                                 sudo apt install libhdf5-serial-dev -y || true
                                 sudo apt install python3-dev python3-tk python-imaging-tk curl cuda-keyring gnupg-agent dirmngr alsa-utils -y || true
                                 sudo apt install libgtk-3-dev -y || true
-                                sudo apt update
+                                sudo apt update -y
                                 sudo dirmngr </dev/null
                                 if sudo apt-add-repository -y ppa:graphics-drivers/ppa && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FCAE110B1118213C; then
                                     echo "Alternative method succeeded."
@@ -65,10 +66,10 @@ else
                                     sudo chmod 644 /etc/apt/trusted.gpg.d/graphics-drivers.gpg
                                 fi
                                 sudo ubuntu-drivers autoinstall
-                                sudo apt update
+                                sudo apt update -y
                                 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
                                 sudo dpkg -i cuda-keyring_1.1-1_all.deb
-                                sudo apt update
+                                sudo apt update -y
                                 sudo apt -y install cuda-toolkit
                                 export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
                                 sudo apt-get update
@@ -79,7 +80,8 @@ else
                                 sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
                                 sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
                                 sudo apt install linux-headers-$(uname -r) -y
-                                sudo apt-key del 7fa2af80
+                                sudo apt del 7fa2af80 || true
+                                sudo apt remove 7fa2af80 || true
                                 sudo apt install build-essential cmake gpg unzip pkg-config software-properties-common ubuntu-drivers-common -y
                                 sudo apt install libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev -y
                                 sudo apt install libjpeg-dev libpng-dev libtiff-dev -y 
@@ -89,7 +91,7 @@ else
                                 sudo apt install libhdf5-serial-dev -y 
                                 sudo apt install python3-dev python3-tk curl gnupg-agent dirmngr alsa-utils -y
                                 sudo apt install libgtk-3-dev -y 
-                                sudo apt update
+                                sudo apt update -y
                                 sudo dirmngr </dev/null
                                 if sudo apt-add-repository -y ppa:graphics-drivers/ppa && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FCAE110B1118213C; then
                                     echo "Alternative method succeeded."
@@ -101,20 +103,21 @@ else
                                     sudo chmod 644 /etc/apt/trusted.gpg.d/graphics-drivers.gpg
                                 fi
                                 sudo ubuntu-drivers autoinstall
-                                sudo apt update
+                                sudo apt update -y
                                 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
                                 sudo dpkg -i cuda-keyring_1.1-1_all.deb
-                                sudo apt update
+                                sudo apt update -y
                                 sudo apt -y install cuda-toolkit
                                 export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-                                sudo apt update
+                                sudo apt update -y
                                 ;;
 
                             "18.04")
                                 # Commands specific to Ubuntu 18.04
                                 sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
                                 sudo apt-get install linux-headers-$(uname -r) -y
-                                sudo apt-key del 7fa2af80
+                                sudo apt del 7fa2af80 || true
+                                sudo apt remove 7fa2af80 || true
                                 sudo apt install build-essential cmake gpg unzip pkg-config software-properties-common ubuntu-drivers-common alsa-utils -y
                                 sudo apt install libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev -y || true
                                 sudo apt install libjpeg-dev libpng-dev libtiff-dev -y || true
@@ -124,15 +127,15 @@ else
                                 sudo apt install libhdf5-serial-dev -y || true
                                 sudo apt install python3-dev python3-tk python-imaging-tk curl cuda-keyring -y || true
                                 sudo apt install libgtk-3-dev -y || true
-                                sudo apt update
+                                sudo apt update -y
                                 sudo ubuntu-drivers install
-                                sudo apt update
+                                sudo apt update -y
                                 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
                                 sudo dpkg -i cuda-keyring_1.1-1_all.deb
-                                sudo apt update
+                                sudo apt update -y
                                 sudo apt -y install cuda-toolkit
                                 export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-                                sudo apt update
+                                sudo apt update -y
                                 ;;
 
                             *)
@@ -148,11 +151,11 @@ else
                                 # Commands specific to Debian 10 & 11
                                 sudo -- sh -c 'apt update; apt upgrade -y; apt autoremove -y; apt autoclean -y'
                                 sudo apt install linux-headers-$(uname -r) -y
-                                sudo apt update
+                                sudo apt update -y
                                 sudo apt install nvidia-driver firmware-misc-nonfree
                                 wget https://developer.download.nvidia.com/compute/cuda/repos/debian${VERSION}/x86_64/cuda-keyring_1.1-1_all.deb
                                 sudo apt install nvidia-cuda-dev nvidia-cuda-toolkit
-                                sudo apt update
+                                sudo apt update -y
                                 ;;
 
                             *)
@@ -200,75 +203,50 @@ if command -v docker &>/dev/null; then
     echo "Docker is already installed."
 else
     echo "Docker is not installed. Proceeding with installations..."
-# Install Docker-ce keyring
-sudo apt update
-sudo apt install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    # Install Docker-ce keyring
+    sudo apt update -y
+    sudo apt install -y ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    FILE=/etc/apt/keyrings/docker.gpg
+    if [ -f "$FILE" ]; then
+        sudo rm "$FILE"
+    fi
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o "$FILE"
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Add Docker-ce repository to Apt sources and install
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce
+    # Add Docker-ce repository to Apt sources and install
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release; echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update -y
+    sudo apt -y install docker-ce
 fi
 
 # Check if docker-compose is installed
 if command -v docker-compose &>/dev/null; then
-    echo "Docker-compoose is already installed."
+    echo "Docker-compose is already installed."
 else
     echo "Docker-compose is not installed. Proceeding with installations..."
 
-# Install docker-compose subcommand
-sudo apt install docker-compose-plugin
-sudo ln -sv /usr/libexec/docker/cli-plugins/docker-compose /usr/bin/docker-compose
-docker-compose --version
+    # Install docker-compose subcommand
+    sudo apt -y install docker-compose-plugin
+    sudo ln -sv /usr/libexec/docker/cli-plugins/docker-compose /usr/bin/docker-compose
+    docker-compose --version
+fi
 
 # Test / Install nvidia-docker
 if [[ ! -z "$NVIDIA_PRESENT" ]]; then
-    if sudo docker run --gpus all nvidia/cuda:12.0.0-base-ubuntu$VERSION_ID nvidia-smi &>/dev/null; then
+    if sudo docker run --gpus all nvidia/cuda:11.0.3-base-ubuntu18.04 nvidia-smi &>/dev/null; then
         echo "nvidia-docker is enabled and working. Exiting script."
     else
         echo "nvidia-docker does not seem to be enabled. Proceeding with installations..."
-	# Refer to install guide: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#installing-with-apt
-        curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-		&& curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
-		sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-		sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-  	
-   	# The nvidia-ctk command modifies the /etc/docker/daemon.json file on the host. 
-    	# The file is updated so that Docker can use the NVIDIA Container Runtime.
-      	sudo nvidia-ctk runtime configure --runtime=docker 
+        distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+        curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add
+        curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
         sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
         sudo systemctl restart docker 
-
-# Install/Test Docker NVIDIA Image for Active Distro
- 	case $DISTRO in
-	    "ubuntu")
-            case $VERSION in
-                	"22.04")
-			        sudo docker run --gpus all nvidia/cuda:12.0.0-base-ubuntu22.04 nvidia-smi
-			        echo "Installation successful !"
-			        ;;
-            esac
-	    ;;
-		        "20.04")
-			        sudo docker run --gpus all nvidia/cuda:12.0.0-base-ubuntu20.04 nvidia-smi
-			        echo "Installation successful !"
-			        ;;
-
-                	"18.04")
-			        sudo docker run --gpus all nvidia/cuda:12.0.0-base-ubuntu18.04 nvidia-smi
-			        echo "Installation successful !"
-			        ;;
-		        *)
-			        echo "Your OS is not supported"
-			        exit 1
-			        ;;
-        esac
+        sudo docker run --gpus all nvidia/cuda:11.0.3-base-ubuntu18.04 nvidia-smi
     fi
 fi
 
